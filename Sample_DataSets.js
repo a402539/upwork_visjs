ed5f1da10347c677a8ca927b39b87173fe0745df
Boris <a402539@yandex.ru>
2021-04-16 19:15:04 +0300

    initial

---
+++ Sample_DataSets.js
@@ -0,0 +1,59 @@
+ var courseDataSet = [{
+         id: 1,
+         label: "Economics 101",
+         title: "A beginners guide to Economics",
+         relatedFunctions: {
+             1: "mathematics",
+             2: "logistics",
+             3: "technology",
+         },
+         schoolLocation: "Melbourne",
+         courseOwner: "School of Finance",
+         deliveryMode: "Face-to-face"
+     },
+     {
+         id: 2,
+         label: "Advanced Marketing",
+         title: "Techniques and Tools to progress customer pipelines",
+         relatedFunctions: {
+             1: "technology",
+             2: "human resources",
+             3: "psychology",
+         },
+         schoolLocation: "Melbourne",
+         courseOwner: "School of Marketing",
+         deliveryMode: "Online",
+     }
+ ];
+ var degreeDataSet = [{
+     id: 1,
+     label: "Degree in Economics",
+     Title: "This prepares you for a job in finance",
+     relatedCourses: {
+         1: "Economics 101",
+         2: "Stockmarkets 101",
+         3: "Advanced Algebra",
+     },
+     degreeOwner: "School of Finance",
+ }];
+ var collegeDataSet = [{
+         id: 1,
+         label: "College of Business",
+         Title: "The premier College for all things business",
+         relatedSchools: {
+             1: "School of Finance",
+             2: "School of Marketing",
+             3: "School of Human Resourcing",
+         }
+     },
+     {
+         id: 2,
+         label: "College of Law",
+         Title: "Australia's best legal education",
+         relatedSchools: {
+             1: "School of Law",
+             2: "School of International Relations",
+             3: "School of Human Rights and Ethics",
+         }
+     }
+ ];
